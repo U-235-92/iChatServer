d969f668f78347e80906d3f511484a38783c1586
U-235-92 <a745296@yandex.ru>
2022-08-24 17:02:50 +0400

    init

--- src/main/java/aq/koptev/i/ServerApp.java
+++ src/main/java/aq/koptev/i/ServerApp.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class ServerApp {
 
     public static void main(String[] args) {
+
         try {
             Server server = new Server();
             server.launch();
